67f517f80f3fa69ca372b8c5527d6a4a99368432
Juan Miguel <juanmiguelmaestre4@gmail.com>
2026-03-23 12:33:05 -0400

    Pushing pipelines updates missing on remote repo

--- src/infrastructure/futbolCrawler/futbolCrawler/pipelines.py
+++ src/infrastructure/futbolCrawler/futbolCrawler/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import re
 
 class FutbolcrawlerPipeline:
     def process_item(self, item, spider):
-        return item
+
+        if item.get("texto_noticia"):
+            item["texto_noticia"] = self.clean_text(item["texto_noticia"]) 
+            item['titular'] = self.clean_text(item['texto_noticia'])
+
+        return item 
+
+    def clean_text(self, text):
+        
+        #Allow all this 
+        text = re.sub(r'[^a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\s\.\,\:\;\"\'\!\?\¿\¡\-\+\(\)\@\$\#\[\]]', '', text)
+        #Remove multiple spaces and odd line breaks.
+        text = re.sub(r'\s+', ' ', text)
+
+        return text.strip()
